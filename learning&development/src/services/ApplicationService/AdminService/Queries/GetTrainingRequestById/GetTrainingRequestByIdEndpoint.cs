587ea96a59adea1b11f068f14adca1ce7a71a252
AbdulMuqtadeer.Mohd <abdulmuqtadeer.mohd@ust.com>
2024-09-23 11:22:57 +0530

    modify GettrainingRequest endpoint

---
+++ learning&development/src/services/ApplicationService/AdminService/Queries/GetTrainingRequestById/GetTrainingRequestByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Carter;
+using MediatR;
+
+namespace ApplicationService.AdminService.Queries.GetTrainingRequestById
+{
+    public class GetTrainingRequestByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/training-requests/{id:int}", async (int id, IMediator mediator) =>
+            {
+                var query = new GetSingleTrainingRequestQuery(id); 
+                try
+                {
+                    var trainingRequest = await mediator.Send(query);
+                    return Results.Ok(trainingRequest);
+                }
+                catch (KeyNotFoundException ex)
+                {
+                    return Results.NotFound(new { message = ex.Message });
+                }
+            });
+        }
+    }
+}
---
+++ learning&development/src/services/ApplicationService/AdminService/Queries/GetTrainingRequestById/GetTrainingRequestByIdHandler.cs
@@ -0,0 +1,39 @@
+﻿using AuthAPI.Data;
+using AuthAPI.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationService.AdminService.Queries.GetTrainingRequestById
+{
+    public class GetSingleTrainingRequestQuery : IRequest<TrainingRequest>
+    {
+        public int Id { get; set; }
+
+        public GetSingleTrainingRequestQuery(int id)
+        {
+            Id = id;
+        }
+    }
+    public class GetTrainingRequestByIdHandler : IRequestHandler<GetSingleTrainingRequestQuery, TrainingRequest>
+    {
+        private readonly AuthDbContext _context;
+
+        public GetTrainingRequestByIdHandler(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TrainingRequest> Handle(GetSingleTrainingRequestQuery request, CancellationToken cancellationToken)
+        {
+            var trainingRequest = await _context.TrainingRequests
+                .FirstOrDefaultAsync(tr => tr.Id == request.Id, cancellationToken);
+
+            if (trainingRequest == null)
+            {
+                throw new KeyNotFoundException($"Training request with ID {request.Id} not found.");
+            }
+
+            return trainingRequest;
+        }
+    }
+}
